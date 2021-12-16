ae57b0aca29eea37e2e9a06432b5245fd9ea589a
TarikElha <www.elhantiti.t@gmail.com>
2021-12-16 11:08:48 +0100

    df

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -5,10 +5,13 @@ namespace App\Entity;
 use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @Assert\EnableAutoMapping()
+ * @UniqueEntity(fields={"title"}, message="Cette série existe déjà.")
  */
 class Program
 {
@@ -21,11 +24,17 @@ class Program
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez renseigner un titre.")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="Veuillez renseigner la synopsis de la série.")
+     * @Assert\Regex(
+     *      pattern="/\W(plus belle la vie)/",
+     *      message="On parle de vraies séries ici !"
+     * )
      */
     private $summary;
 
